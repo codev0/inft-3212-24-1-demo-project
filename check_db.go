88aee87d8d4ba17d6b6eb171ea7b5d67cbf3da4a
Airat Zhanshuakov <dakanca@gmail.com>
2024-03-30 09:40:50 +0500

    chore: add air livereload

--- check_db.go
+++ check_db.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
 	_ "github.com/lib/pq"
 )
 
@@ -12,13 +8,15 @@ func main() {
 	// DSN string anatomy
 	// Linux/Mac: postgres://username:password@host:port/database?sslmode=disable
 	// Windows: postgresql://username:password@host:port/database?sslmode=disable
-	db, err := sql.Open("postgres", "postgres://codev0:pa55word@localhost:5432/lecture6?sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to DB!")
+
+	// Uncomment the following code to check if the database connection is working
+	// db, err := sql.Open("postgres", "postgres://codev0:pa55word@localhost:5432/lecture6?sslmode=disable")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Connected to DB!")
 }
